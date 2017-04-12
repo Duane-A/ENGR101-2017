2e0e245d963deaed86a786ba472bf4177338ddb3
Duane-A <alcaladuan@myvuw.ac.nz>
2017-04-13 11:00:27 +1200

    Create move.cpp

---
+++ move.cpp
@@ -0,0 +1,44 @@
+include <stdio.h>
+include <time.h>
+include "ECS100"
+
+int main(){
+  init();
+  
+  int adc_reading;
+  int threshold;
+  
+  if(adc_reading < threshold>{
+    reverse(500);
+  }
+
+}
+
+//Drives robot straight
+void straight(duratiom){
+  set_motor(1,255);
+  set_motor(2,255);
+  sleep1(0, duration);
+}
+
+//Turns robot left
+void left(duration){
+  set_motor(1,0);
+  set_motor(2,255);
+  sleep1(0, duration);
+}
+
+//Turns robot right
+void right(){
+  set_motor(1,255);
+  set_motor(2,0);
+  sleep1(0, duration);
+}
+
+//Reverses robot
+void reverse(duration){
+  set_motor(1,-127);
+  set_motor(2,-127);
+  sleep1(0, duration);
+}
+
