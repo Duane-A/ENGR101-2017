a980b40e72bbfc96d3044f49c2137764f52355d3
Duane-A <alcaladuan@myvuw.ac.nz>
2017-04-13 11:03:44 +1200

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -8,35 +8,35 @@ int main(){
   int adc_reading;
   int threshold;
   
-  if(adc_reading < threshold>{
+  if(adc_reading < threshold){
     reverse(500);
   }
   return 0;
 }
 
 //Drives robot straight
-void straight(duratiom){
+void straight(int duratiom){
   set_motor(1,255);
   set_motor(2,255);
   sleep1(0, duration);
 }
 
 //Turns robot left
-void left(duration){
+void left(int duration){
   set_motor(1,0);
   set_motor(2,255);
   sleep1(0, duration);
 }
 
 //Turns robot right
-void right(){
+void right(int duration){
   set_motor(1,255);
   set_motor(2,0);
   sleep1(0, duration);
 }
 
 //Reverses robot
-void reverse(duration){
+void reverse(int duration){
   set_motor(1,-127);
   set_motor(2,-127);
   sleep1(0, duration);
